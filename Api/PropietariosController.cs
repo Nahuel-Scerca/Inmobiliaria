26bda1a4f63ce1e7e1c599fc137fae23ae24ab39
Nahuel-Scerca <56772334+Nahuel-Scerca@users.noreply.github.com>
2020-10-17 18:12:03 +0200

    Api

---
+++ Api/PropietariosController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebApplicationPrueba.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplicationPrueba.Api
+{
+    [Route("api/[controller]")]
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [ApiController]
+    public class PropietariosController : Controller
+    {
+        private readonly DataContext contexto;
+        private readonly IConfiguration config;
+
+        public PropietariosController(DataContext contexto, IConfiguration config)
+        {
+            this.contexto = contexto;
+            this.config = config;
+        }
+        // GET: api/<controller>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                /*contexto.Inmuebles
+                    .Include(x => x.Duenio)
+                    .Where(x => x.Duenio.Nombre == "")//.ToList() => lista de inmuebles
+                    .Select(x => x.Duenio)
+                    .ToList();//lista de propietarios*/
+                var usuario = User.Identity.Name;
+                var res = contexto.Propietarios.Select(x => new  {x.Nombre, x.Apellido, x.Email }).SingleOrDefault(x => x.Email == usuario);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                return Ok(contexto.Propietarios.SingleOrDefault(x => x.Id == id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        // GET api/<controller>/5
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginView loginView)
+        {
+            try
+            {
+                string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                    password: loginView.Clave,
+                    salt: System.Text.Encoding.ASCII.GetBytes(config["Salt"]),
+                    prf: KeyDerivationPrf.HMACSHA1,
+                    iterationCount: 1000,
+                    numBytesRequested: 256 / 8));
+                var p = contexto.Propietarios.FirstOrDefault(x => x.Email == loginView.Usuario);
+                if (p == null || p.Clave != hashed)
+                {
+                    return BadRequest("Nombre de usuario o clave incorrecta");
+                }
+                else
+                {
+                    var key = new SymmetricSecurityKey(System.Text.Encoding.ASCII.GetBytes(config["TokenAuthentication:SecretKey"]));
+                    var credenciales = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, p.Email),
+                        new Claim("FullName", p.Nombre + " " + p.Apellido),
+                        new Claim(ClaimTypes.Role, "Propietario"),
+                    };
+
+                    var token = new JwtSecurityToken(
+                        issuer: config["TokenAuthentication:Issuer"],
+                        audience: config["TokenAuthentication:Audience"],
+                        claims: claims,
+                        expires: DateTime.Now.AddMinutes(60),
+                        signingCredentials: credenciales
+                    );
+                    return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+                }                
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public async Task<IActionResult> Post(Propietario entidad)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    contexto.Propietarios.Add(entidad);
+                    contexto.SaveChanges();
+                    return CreatedAtAction(nameof(Get), new { id = entidad.Id }, entidad);
+                }
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+            //contexto.Propietarios.Update()
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        // GET: api/<controller>
+        [HttpGet("test")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Test()
+        {
+            try
+            {
+                return Ok("anduvo");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
