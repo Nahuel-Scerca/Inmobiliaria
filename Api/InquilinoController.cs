dd414d08f880be850d5a5c091814ab99f4b4a899
Nahuel-Scerca <56772334+Nahuel-Scerca@users.noreply.github.com>
2020-10-20 13:14:51 +0200

    Controladores Api

---
+++ Api/InquilinoController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationPrueba.Models;
+
+namespace WebApplicationPrueba.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InquilinoController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public InquilinoController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Inquilino
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Inquilino>>> Get()
+        {
+            return await _context.Inquilinos.ToListAsync();
+        }
+
+        // GET: api/Inquilino/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Inquilino>> Get(int id)
+        {
+            var inquilino = await _context.Inquilinos.FindAsync(id);
+
+            if (inquilino == null)
+            {
+                return NotFound();
+            }
+
+            return inquilino;
+        }
+
+        // PUT: api/Inquilino/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Inquilino inquilino)
+        {
+            if (id != inquilino.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(inquilino).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InquilinoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Inquilino
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Inquilino>> Post(Inquilino inquilino)
+        {
+            _context.Inquilinos.Add(inquilino);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetInquilino", new { id = inquilino.Id }, inquilino);
+        }
+
+        // DELETE: api/Inquilino/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Inquilino>> Delete(int id)
+        {
+            var inquilino = await _context.Inquilinos.FindAsync(id);
+            if (inquilino == null)
+            {
+                return NotFound();
+            }
+
+            _context.Inquilinos.Remove(inquilino);
+            await _context.SaveChangesAsync();
+
+            return inquilino;
+        }
+
+        private bool InquilinoExists(int id)
+        {
+            return _context.Inquilinos.Any(e => e.Id == id);
+        }
+    }
+}
